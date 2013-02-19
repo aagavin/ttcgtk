1f9e965a7c0aed958d8797b7025342a393dc222d
aagavin <aagavin@gmail.com>
2013-02-18 19:38:00 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+from gi.repository import Gtk, Gdk#, GdkPixbuf
+from parser import ParseData
+#
+class Appgui:
+	"""docstring for Appgui"""
+	def __init__(self):
+		#builder init stuff
+		builder=Gtk.Builder()
+		builder.add_from_file("ttc.glade")
+		builder.connect_signals(Signals())
+		#window
+		#self.window=builder.get_object("main_window")
+
+		### combo boxes
+		#route_combo
+		self.route_cmb=builder.get_object("route_combo")
+		#direction_combo
+		self.dir_cmb=builder.get_object("direction_combo")
+		#stop_combo
+		self.stop_cmb=builder.get_object("stop_combo")
+		###
+
+		#show all
+		#self.window.show_all()
+		builder.get_object("main_window").show_all()
+		#generate
+		self.generate()
+
+	""" generate """
+	def generate(self):
+		for i in ParseData().getAllRoutesTitle():
+			self.route_cmb.append_text(str(i))
+
+class Signals:
+	"""docstring for Signals"""
+
+	def gtk_close(self,widget,event=None):
+		Gtk.main_quit()
+
+	def generateDirection(self,widget,event=None):
+		busnum=hello.route_cmb.get_active_text().split("-")[0]
+		hello.dir_cmb.get_model().clear()
+		for i in ParseData().getRouteDirection(busnum):
+			hello.dir_cmb.append_text(str(i))
+
+	def generateStop(self,widget,event=None):
+		busnum=hello.route_cmb.get_active_text().split("-")[0]
+		dirNum=hello.dir_cmb.get_active()
+
+		hello.stop_cmb.get_model().clear()
+		for i in ParseData().getStops(busnum,dirNum):
+			hello.stop_cmb.append_text(str(i))
+
+		
+		
+if __name__=="__main__":
+	hello=Appgui()
+	Gtk.main()
---
+++ parser.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+#
+
+#import library to do http requests:
+import urllib
+#import easy to use xml parser called minidom:
+from xml.dom import minidom
+
+#All Routes List Data url
+ROUTESLISTURL='http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc'
+DIRECTIONTAGINFO ='http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r='
+
+class ParseData:
+	"""The class that gets the
+	requred data and returns it"""
+	def __init__(self):
+		pass
+
+	def getAllRoutesTitle(self):
+		routeArray=[]
+		dom=minidom.parse(urllib.urlopen(ROUTESLISTURL))
+		for i in dom.getElementsByTagName('route'):
+			routeArray.append(i.attributes['title'].value)
+		return routeArray
+
+
+	def getRouteDirection(self,route):
+		directionarr=[]
+		dom=minidom.parse(urllib.urlopen(DIRECTIONTAGINFO+route))
+		for i in dom.getElementsByTagName('direction'):
+			directionarr.append(i.attributes['title'].value)
+		return directionarr
+
+	def getStops(self,route,dirNum):
+		stopArr=[]
+		stopStr=""
+		directions=self.getRouteDirection(route)
+
+		dom=minidom.parse(urllib.urlopen(DIRECTIONTAGINFO+route))
+
+		for i in dom.getElementsByTagName('stop'):
+			try:
+				title=i.attributes['title'].value
+				if "_ar" in i.attributes['tag'].value:
+					stopStr=stopStr+" "+title+"|"
+				else:
+					stopStr=stopStr+" "+title+"!"
+				#stopArr.append(stop)
+			except Exception:
+				continue
+
+		#print len(stopStr.split("|"))
+
+		print stopStr.split("|")[dirNum]
+
+
+		return stopArr
